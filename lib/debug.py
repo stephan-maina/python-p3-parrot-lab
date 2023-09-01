43e5366dbf8d6b23c8a5b8c2ff8f581321984cae
Mr.Perfect 092 <135037455+stephan-maina@users.noreply.github.com>
2023-09-02 02:34:49 +0300

    Update debug.py

--- lib/debug.py
+++ lib/debug.py
@@ -1,6 +1,27 @@
-#!/usr/bin/env python3
+# debug.py
 
-from parrot import parrot
+def divide_numbers(a, b):
+    """
+    Divide two numbers.
 
-if __name__ == '__main__':
-    import ipdb; ipdb.set_trace()
+    Args:
+    a (float): The dividend.
+    b (float): The divisor.
+
+    Returns:
+    float: The result of the division.
+    """
+    result = a / b
+    return result
+
+# Triggering a bug
+if __name__ == "__main__":
+    numerator = 10
+    denominator = 0  # This will cause a ZeroDivisionError
+
+    try:
+        result = divide_numbers(numerator, denominator)
+        print(f"Result: {result}")
+    except ZeroDivisionError as e:
+        import pdb
+        pdb.post_mortem()
